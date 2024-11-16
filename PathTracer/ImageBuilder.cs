31bcba04c5a0a4a5740efc540440db17cd3c761a
Jort Willemsen <jort@maurice-nienke.nl>
2024-11-16 12:57:26 +0100

    Fix: type renaming

--- PathTracer/ImageBuilder.cs
+++ PathTracer/ImageBuilder.cs
@@ -13,17 +13,17 @@ public static class ImageBuilder
         lines[1] = width + " " + height;
         lines[2] = "255";
 
-        for (int j = 0; j < height; j++)
+        for (var j = 0; j < height; j++)
         {
-            for (int i = 0; i < width; i++)
+            for (var i = 0; i < width; i++)
             {
                 var r = decimal.ToDouble(i) / (width - 1);
                 var g = decimal.ToDouble(j) / (height - 1);
                 var b = 0.0d;
 
-                int ir = (int) (255.999 * r);
-                int ig = (int) (255.999 * g);
-                int ib = (int) (255.999 * b);
+                var ir = (int) (255.999 * r);
+                var ig = (int) (255.999 * g);
+                var ib = (int) (255.999 * b);
         
                 lines[j * width + 3 + i] = ir + " " + ig + " " +  ib;
             }
@@ -36,7 +36,7 @@ public static class ImageBuilder
     {
         var lines = Build(width, height);
         Console.WriteLine(Directory.GetCurrentDirectory());
-        using StreamWriter outputFile = new StreamWriter(Directory.GetCurrentDirectory() + filePath);
+        using var outputFile = new StreamWriter(Directory.GetCurrentDirectory() + filePath);
         foreach (var line in lines)
         {
             outputFile.WriteLine(line);
